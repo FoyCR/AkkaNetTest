a05dc270be3c9d61c7ce53b1566b313959048a21
Foy <ryocr000@gmail.com>
2024-03-04 10:37:32 -0600

    Update instructions
    Updated to prompt the user to provide a valid path to a log file.

--- ConsoleReaderActor.cs
+++ ConsoleReaderActor.cs
@@ -1,5 +1,4 @@
 ﻿using Akka.Actor;
-using System.ComponentModel.DataAnnotations;
 
 namespace AkkaNetConsole
 {
@@ -19,7 +18,7 @@ namespace AkkaNetConsole
             if (rawMessage.Equals(StartCommand))
             {
                 printInstructions();
-            }          
+            }
 
             GetAndValidateInput();
         }
@@ -27,7 +26,7 @@ namespace AkkaNetConsole
         private void GetAndValidateInput()
         {
             var message = Console.ReadLine();
-            
+
             if (!string.IsNullOrEmpty(message) && string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
             {
                 Context.System.Terminate();
@@ -38,9 +37,7 @@ namespace AkkaNetConsole
 
         private void printInstructions()
         {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
-            Console.WriteLine($"Type '{ExitCommand}' to quit this application at any time.\n");
+            Console.WriteLine("Please provide the URI of a log file on disk to tail.\n");
         }
     }
 }
